5dfc57995282c55cea665dea908c34897f29e453
grunkleij <indrajithpg10@gmail.com>
2025-08-10 20:44:39 +0530

    java config added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,10 +12,16 @@ return {
     },
   },
   {
+    'nvim-java/nvim-java',
+    config = function()
+      require('java').setup()
+    end
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "tailwindcss" },
+        ensure_installed = { "lua_ls", "ts_ls", "tailwindcss", },
       })
     end
   },
@@ -97,6 +103,10 @@ return {
       })
 
       lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.jdtls.setup({
         capabilities = capabilities,
       })
 
